6c857c4d1cea101d5a60c1bd3f7854260868747c
Noguchi <19ek084@ms.dendai.ac.jp>
2021-10-29 22:25:49 +0900

    add noguchi laravel

---
+++ noguchi_laravel_test/testsight/resources/views/top.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+</hand>
+<body>
+<header>
+    <h1>Company Site<h1>
+    <nav>
+        <a href="/">トップ</a>
+    </nav>
+</header>
+<main>
+    <h1>これはトップページだよ</h1>
+</main>
+<footer>
+    &copy; 2020 xxxx
+</footer>
+</body>
+</html>
