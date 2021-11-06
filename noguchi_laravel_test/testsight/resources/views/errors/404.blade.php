94bdcb38e490ad8c0d1619c2acc6751f1947c9c6
Noguchi <19ek084@ms.dendai.ac.jp>
2021-11-06 13:19:43 +0900

    update noguchi_laravel add error page

---
+++ noguchi_laravel_test/testsight/resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@include("parts.common")
+@include("parts.header")
+<main>
+	<div style="text-align:center">
+		<h1>ページが見つかりません.ドンマイ．</h1>
+		<a href="{{ url('/') }}">トップに戻る</a>
+	</div>
+</main>
+@include("parts.footer")
+</body>
+</html>
