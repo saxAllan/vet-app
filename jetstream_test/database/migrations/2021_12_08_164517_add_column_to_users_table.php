67da8b515ed097d1f90a5d502f94afa29163cbc8
asahi <saxAllan>
2021-12-16 00:59:41 +0900

    upload jetstream_test

---
+++ jetstream_test/database/migrations/2021_12_08_164517_add_column_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('hurigana')->nullable();
+            $table->string('tel')->nullable();
+            $table->string('post')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+		$table->dropColumn('hurigana');
+		$table->dropColumn('tel');
+                $table->dropColumn('post');
+        });
+    }
+}
